0dea1bd608cd8789403ccba32bbeebbdf6df7028
Matěj Jelínek <Matej.Jelinek@assecosol.com>
2023-05-12 19:11:49 +0200

    update

--- main.py
+++ main.py
@@ -1,94 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import PIL
 import tensorflow as tf
 
 from tensorflow import keras
 from keras import layers
 from keras.models import Sequential
 
-import pathlib
 
-def prepareDataset():
-    dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-    data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
-    print(pathlib.Path(data_dir))
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    #prepareDataset()
-    data_dir = "C:\\Users\\rackh_6j8312r\\.keras\\datasets\\flower_photos"
-    batch_size = 32
-    img_height = 180
-    img_width = 180
-
-    train_ds = tf.keras.utils.image_dataset_from_directory(
-        data_dir,
-        validation_split=0.2,
-        subset="training",
-        seed=123,
-        image_size=(img_height, img_width),
-        batch_size=batch_size)
-
-    val_ds = tf.keras.utils.image_dataset_from_directory(
-        data_dir,
-        validation_split=0.2,
-        subset="validation",
-        seed=123,
-        image_size=(img_height, img_width),
-        batch_size=batch_size)
-
-    class_names = train_ds.class_names
-    print(class_names)
-
-    AUTOTUNE = tf.data.AUTOTUNE
-
-    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-
-    normalization_layer = layers.Rescaling(1. / 255)
-
-    normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-    image_batch, labels_batch = next(iter(normalized_ds))
-    first_image = image_batch[0]
-    # Notice the pixel values are now in `[0,1]`.
-    print(np.min(first_image), np.max(first_image))
-
-    num_classes = len(class_names)
-    model = Sequential([
-        layers.Rescaling(1. / 255, input_shape=(img_height, img_width, 3)),
-        layers.Conv2D(16, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(32, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(64, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Flatten(),
-        layers.Dense(128, activation='relu'),
-        layers.Dense(num_classes)
-    ])
-
-    model.compile(optimizer='adam',
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-
-    model.summary()
-
-    epochs = 10
-    history = model.fit(
-        train_ds,
-        validation_data=val_ds,
-        epochs=epochs
-    )
+def plot_training_reusult(epochs, history):
+    epochs_range = range(epochs)
 
     acc = history.history['accuracy']
     val_acc = history.history['val_accuracy']
 
     loss = history.history['loss']
     val_loss = history.history['val_loss']
-
-    epochs_range = range(epochs)
 
     plt.figure(figsize=(8, 8))
     plt.subplot(1, 2, 1)
@@ -103,3 +29,114 @@ if __name__ == '__main__':
     plt.legend(loc='upper right')
     plt.title('Training and Validation Loss')
     plt.show()
+
+
+def create_dataset(directory, height, width, batch, subset):
+    return tf.keras.utils.image_dataset_from_directory(
+            directory,
+            validation_split=0.2,
+            subset=subset,
+            seed=123,
+            image_size=(height, width),
+            batch_size=batch)
+
+
+def predict_img(model, class_names, path, height, width):
+    img = tf.keras.utils.load_img(
+        path, target_size=(height, width)
+    )
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
+    img_array = tf.keras.applications.resnet50.preprocess_input(img_array)
+    prediction = model.predict(img_array)
+
+    # normalization of confidence
+    prediction = tf.nn.softmax(prediction)
+
+    prediction_class = np.argmax(prediction)
+    print(
+        "This image most likely belongs to {} with confidence {:.2f}"
+        .format(class_names[prediction_class], prediction[0][prediction_class])
+    )
+
+if __name__ == '__main__':
+    data_dir = "dataset"
+    training_dir = "\\training_set"
+    validation_dir = "\\validation_set"
+    test_valid_path = "cactus.jpg"
+    test_no_valid_path = "no_cactus.jpg"
+
+    model_path = "model.tflite"
+
+    batch_size = 32
+    img_height = 180
+    img_width = 180
+
+    class_names = ["cactus", "no_cactus"]
+    epochs = 3
+
+    train_test = False  # True = train & test, False = test only
+
+    if train_test:
+        train_ds = create_dataset(data_dir, img_height, img_width, batch_size, "training")
+
+        validation_ds = create_dataset(data_dir, img_height, img_width, batch_size, "validation")
+
+        # optimization
+        train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+        validation_ds = validation_ds.cache().prefetch(buffer_size=tf.data.AUTOTUNE)
+
+        # normalization
+        normalization_layer = layers.Rescaling(1. / 255)
+        normalized_train_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+        normalized_validation_ds = validation_ds.map(lambda x, y: (normalization_layer(x), y))
+
+        # augmentation by fliping images
+        data_augmentation = keras.Sequential(
+            [
+                layers.RandomFlip("horizontal",
+                                  input_shape=(img_height,
+                                               img_width,
+                                               3)),
+                layers.RandomRotation(0.1),
+                layers.RandomZoom(0.1),
+            ]
+        )
+
+        model = Sequential([
+            data_augmentation,
+            layers.Rescaling(1. / 255, input_shape=(img_height, img_width, 3)),
+            layers.Conv2D(16, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Conv2D(32, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Conv2D(64, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Dropout(0.2),
+            layers.Flatten(),
+            layers.Dense(128, activation='relu'),
+            layers.Dense(len(class_names))
+        ])
+
+        model.compile(optimizer='adam',
+                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                      metrics=['accuracy'])
+
+        model.summary()
+
+        history = model.fit(
+            normalized_train_ds,
+            validation_data=normalized_validation_ds,
+            epochs=epochs
+        )
+
+        plot_training_reusult(epochs, history)
+
+        model.save('model.h5')
+
+    interpreter = tf.keras.models.load_model('model.h5')
+
+    print("Should pass as cactus.")
+    predict_img(interpreter, class_names, test_valid_path, img_height, img_width)
+    print("Should not pass as cactus.")
+    predict_img(interpreter, class_names, test_no_valid_path, img_height, img_width)
